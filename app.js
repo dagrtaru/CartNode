acd2f59a1b0b2b640f2d49576b06185e64c257f4
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2019-12-26 16:45:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,101 @@
-let express = require('express');
-let hbs = require('express-handlebars');
+// Database name : cart
+// Table name : items
 
-var app = express();
+const express = require('express');
+const mysql = require('mysql');
 
-app.engine('handlebars', hbs());
-app.set('view engine', 'handlebars');
-
-app.get('/', function(req, res){
-	res.render('home');
+//Create database connection
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database  : 'cart' 
 });
 
-app.listen(3000);
+//Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log("MySql connected");
+});
+
+const app = express();
+
+//Create DB using routing basically
+app.get('/createdb', (req, res) => {
+    
+    sql = 'CREATE DATABASE cart';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database successfully created...');
+    });
+});
+
+//Create table 
+app.get('/createitemstable', (req, res) => {
+    let sql = 'CREATE TABLE items(items_id int AUTO_INCREMENT, title VARCHAR(255), price int, PRIMARY KEY (items_id))';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Items table created...');
+    });
+});
+
+//Insert item
+app.get('/additem1', (req, res) => {
+    let item = {title : 'Shampoo', price : 100};
+    let sql = 'INSERT INTO items SET ?';                    //? acts as a place holder where item will get replaced
+    let query = db.query(sql, item, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send("Item 1 inserted successfully");
+    });
+});
+
+//Insert another item
+app.get('/additem2', (req, res) => {
+    let item = {title : 'Soap', price : 40};
+    let sql = 'INSERT INTO items SET ?';                    //? acts as a place holder where item will get replaced
+    let query = db.query(sql, item, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send("Item 2 inserted successfully");
+    });
+});
+
+//SELECT items
+app.get('/getitems', (req, res) => {
+    let sql = 'SELECT * FROM items';                    
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send("Items fetched...");
+    });
+});
+
+//SELECT single item
+app.get('/getitem/:id', (req, res) => {
+    let sql = `SELECT * FROM items WHERE items_id = ${req.params.id}`;                    
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send("Item fetched...");
+    });
+});
+
+//UPDATE item
+app.get('/updateitem/:id', (req, res) => {
+    let new_title = "Anti-Dandruff Shampoo";
+    let sql = `UPDATE items SET title = '${new_title}' WHERE items_id = ${req.params.id}`;                    
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send("Item updated...");
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server started at port 3000');
+});
